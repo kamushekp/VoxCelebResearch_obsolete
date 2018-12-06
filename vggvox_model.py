1794d5c9d5d4b62e10b0ada21594be94f1b68cab
kamenev.pa <kamenev.pa@skbkontur.ru>
2018-12-06 18:06:05 +0700

    remove redundant args

--- vggvox_model.py
+++ vggvox_model.py
@@ -11,49 +11,45 @@ import constants as c
 
 
 # Block of layers: Conv --> BatchNorm --> ReLU --> Pool
-def conv_bn_pool(inp_tensor,layer_idx,conv_filters,conv_kernel_size,conv_strides,conv_pad,
-    pool='',pool_size=(2, 2),pool_strides=None,
-    conv_layer_prefix='conv'):
-    x = ZeroPadding2D(padding=conv_pad,name='pad{}'.format(layer_idx))(inp_tensor)
-    x = Conv2D(filters=conv_filters,kernel_size=conv_kernel_size, strides=conv_strides, padding='valid', name='{}{}'.format(conv_layer_prefix,layer_idx))(x)
-    x = BatchNormalization(epsilon=1e-5,momentum=1,name='bn{}'.format(layer_idx))(x)
-    x = Activation('relu', name='relu{}'.format(layer_idx))(x)
-    if pool == 'max':
-        x = MaxPooling2D(pool_size=pool_size,strides=pool_strides,name='mpool{}'.format(layer_idx))(x)
-    elif pool == 'avg':
-        x = AveragePooling2D(pool_size=pool_size,strides=pool_strides,name='apool{}'.format(layer_idx))(x)
+def conv_bn_pool(inp_tensor,conv_filters, conv_kernel_size, conv_strides, conv_pad, pool_type = "", pool_size=(2, 2),pool_strides=None):
+    x = ZeroPadding2D(padding=conv_pad)(inp_tensor)
+    x = Conv2D(filters=conv_filters,kernel_size=conv_kernel_size, strides=conv_strides, padding='valid')(x)
+    x = BatchNormalization(epsilon=1e-5,momentum=1)(x)
+    x = Activation('relu')(x)
+    
+    if pool_type == 'max':
+        return MaxPooling2D(pool_size=pool_size,strides=pool_strides)(x)
+    elif pool_type == 'avg':
+        return AveragePooling2D(pool_size=pool_size,strides=pool_strides)(x)
+    
     return x
 
 
 # Block of layers: Conv --> BatchNorm --> ReLU --> Dynamic average pool (fc6 -> apool6 only)
-def conv_bn_dynamic_apool(inp_tensor,layer_idx,conv_filters,conv_kernel_size,conv_strides,conv_pad,
-    conv_layer_prefix='conv'):
-    x = ZeroPadding2D(padding=conv_pad,name='pad{}'.format(layer_idx))(inp_tensor)
-    x = Conv2D(filters=conv_filters,kernel_size=conv_kernel_size, strides=conv_strides, padding='valid', name='{}{}'.format(conv_layer_prefix,layer_idx))(x)
-    x = BatchNormalization(epsilon=1e-5,momentum=1,name='bn{}'.format(layer_idx))(x)
-    x = Activation('relu', name='relu{}'.format(layer_idx))(x)
-    x = GlobalAveragePooling2D(name='gapool{}'.format(layer_idx))(x)
-    x = Reshape((1,1,conv_filters),name='reshape{}'.format(layer_idx))(x)
+def conv_bn_dynamic_apool(inp_tensor,conv_filters,conv_kernel_size,conv_strides,conv_pad):
+    x = ZeroPadding2D(padding=conv_pad)(inp_tensor)
+    x = Conv2D(filters=conv_filters,kernel_size=conv_kernel_size, strides=conv_strides, padding='valid')(x)
+    x = BatchNormalization(epsilon=1e-5,momentum=1)(x)
+    x = Activation('relu')(x)
+    x = GlobalAveragePooling2D()(x)
+    x = Reshape((1,1,conv_filters))(x)
     return x
 
 
 def vggvox_model():
     inp = Input(c.INPUT_SHAPE,name='input')
-    x = conv_bn_pool(inp,layer_idx=1,conv_filters=96,conv_kernel_size=(7,7),conv_strides=(2,2),conv_pad=(1,1),
-        pool='max',pool_size=(3,3),pool_strides=(2,2))
-    x = conv_bn_pool(x,layer_idx=2,conv_filters=256,conv_kernel_size=(5,5),conv_strides=(2,2),conv_pad=(1,1),
-        pool='max',pool_size=(3,3),pool_strides=(2,2))
-    x = conv_bn_pool(x,layer_idx=3,conv_filters=384,conv_kernel_size=(3,3),conv_strides=(1,1),conv_pad=(1,1))
-    x = conv_bn_pool(x,layer_idx=4,conv_filters=256,conv_kernel_size=(3,3),conv_strides=(1,1),conv_pad=(1,1))
-    x = conv_bn_pool(x,layer_idx=5,conv_filters=256,conv_kernel_size=(3,3),conv_strides=(1,1),conv_pad=(1,1),
-        pool='max',pool_size=(5,3),pool_strides=(3,2))
-    x = conv_bn_dynamic_apool(x,layer_idx=6,conv_filters=4096,conv_kernel_size=(9,1),conv_strides=(1,1),conv_pad=(0,0),
-        conv_layer_prefix='fc')
-    x = conv_bn_pool(x,layer_idx=7,conv_filters=1024,conv_kernel_size=(1,1),conv_strides=(1,1),conv_pad=(0,0),
-        conv_layer_prefix='fc')
-    x = Lambda(lambda y: K.l2_normalize(y, axis=3), name='norm')(x)
-    x = Conv2D(filters=1024,kernel_size=(1,1), strides=(1,1), padding='valid', name='fc8')(x)
-    m = Model(inp, x, name='VGGVox')
+    x = conv_bn_pool(inp,conv_filters=96,conv_kernel_size=(7,7),conv_strides=(2,2),conv_pad=(1,1),
+        pool_type='max',pool_size=(3,3),pool_strides=(2,2))
+    x = conv_bn_pool(x,conv_filters=256,conv_kernel_size=(5,5),conv_strides=(2,2),conv_pad=(1,1),
+        pool_type='max',pool_size=(3,3),pool_strides=(2,2))
+    x = conv_bn_pool(x,conv_filters=384,conv_kernel_size=(3,3),conv_strides=(1,1),conv_pad=(1,1))
+    x = conv_bn_pool(x,conv_filters=256,conv_kernel_size=(3,3),conv_strides=(1,1),conv_pad=(1,1))
+    x = conv_bn_pool(x,conv_filters=256,conv_kernel_size=(3,3),conv_strides=(1,1),conv_pad=(1,1), pool_type='max',pool_size=(5,3),pool_strides=(3,2))
+    x = conv_bn_dynamic_apool(x,conv_filters=4096,conv_kernel_size=(9,1),conv_strides=(1,1),conv_pad=(0,0))
+    x = conv_bn_pool(x,conv_filters=1024,conv_kernel_size=(1,1),conv_strides=(1,1),conv_pad=(0,0))
+    x = Lambda(lambda y: K.l2_normalize(y, axis=3))(x)
+    x = Conv2D(filters=1024,kernel_size=(1,1), strides=(1,1), padding='valid')(x)
+    m = Model(inp, x)
     return m
 
 
@@ -75,5 +71,5 @@ def test():
 
 
 if __name__ == '__main__':
-	test()
+    test()
 
